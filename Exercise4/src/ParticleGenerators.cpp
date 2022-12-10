b64f32b9b78ba536f686e233ae96539d351eb32f
xen <selegakamil@gmail.com>
2022-12-10 20:41:43 +0100

    Add SpherePositionGenerator

--- Exercise4/src/ParticleGenerators.cpp
+++ Exercise4/src/ParticleGenerators.cpp
@@ -10,3 +10,16 @@ void BoxPositionGenerator::generate(ParticleData *particles, float dt, size_t st
             ofRandom(origin.z - range.z, origin.z + range.z));
     }
 }
+
+void SpherePositionGenerator::generate(ParticleData *particles, float dt, size_t start_id, size_t end_id)
+{
+    for (size_t i = start_id; i < end_id; ++i)
+    {
+        float theta = ofRandom(0, 2 * PI);
+        float phi = ofRandom(0, 2 * PI);
+        float x = origin.x + radius * sin(theta) * cos(phi);
+        float y = origin.y + radius * sin(theta) * sin(phi);
+        float z = origin.z + radius * cos(theta);
+        particles->position.at(i) = ofVec3f(x, y, z);
+    }
+}
--- Exercise4/src/ofApp.cpp
+++ Exercise4/src/ofApp.cpp
@@ -7,8 +7,12 @@ void ofApp::setup()
     ofSetCircleResolution(100);
 
     particleSystem = ParticleSystem();
+
     particleSystem.addEmitter(std::make_shared<ParticleEmitter>(&particleSystem.particles, 100));
     particleSystem.emitters[0]->addGenerator(std::make_shared<BoxPositionGenerator>(ofVec3f(0, 0, 0), ofVec3f(100, 100, 100)));
+
+    particleSystem.addEmitter(std::make_shared<ParticleEmitter>(&particleSystem.particles, 100));
+    particleSystem.emitters[1]->addGenerator(std::make_shared<SpherePositionGenerator>(ofVec3f(0, 200, 0), 100));
 }
 
 void ofApp::update()
