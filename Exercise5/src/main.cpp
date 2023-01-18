48b9164d57449d8b22faae3bb3b0a85b8bdcd66e
xen <selegakamil@gmail.com>
2023-01-18 18:21:54 +0100

    Change window mode to fullscreen

--- Exercise5/src/main.cpp
+++ Exercise5/src/main.cpp
@@ -4,14 +4,11 @@
 //========================================================================
 int main( ){
 
-	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
 	ofGLWindowSettings settings;
-	settings.setSize(1024, 768);
-	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+	settings.windowMode = OF_GAME_MODE;
 
 	auto window = ofCreateWindow(settings);
 
 	ofRunApp(window, make_shared<ofApp>());
 	ofRunMainLoop();
-
 }
