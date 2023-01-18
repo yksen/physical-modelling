4831ab4ec5fdc74dad9fced928cb358c365f0071
xen <selegakamil@gmail.com>
2023-01-18 18:38:57 +0100

    Add a camera, draw axes and framerate

--- Exercise5/src/ofApp.cpp
+++ Exercise5/src/ofApp.cpp
@@ -1,71 +1,62 @@
 #include "ofApp.hpp"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
-
+void ofApp::setup()
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::update()
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
+    camera.begin();
+    ofDrawAxis(100);
+    camera.end();
 
+    ofDrawBitmapString(ofToString(ofGetFrameRate()), 0, 10);
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseMoved(int x, int y)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mouseDragged(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
+void ofApp::mouseEntered(int x, int y)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
+void ofApp::mouseExited(int x, int y)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+void ofApp::windowResized(int w, int h)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
+void ofApp::gotMessage(ofMessage msg)
+{
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 }
